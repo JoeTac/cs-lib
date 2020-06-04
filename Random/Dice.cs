732195670a4cf057d63e40e074823e4716ac6a50
JoeTac <1260694+JoeTac@users.noreply.github.com>
2020-06-04 14:49:03 -0400

    Update Dice.cs

--- Random/Dice.cs
+++ Random/Dice.cs
@@ -131,34 +131,6 @@ namespace Random
             };
         }
 
-        private class DefaultRandomizer : IRandomizer
-        {
-            System.Random random = null;
-
-            public DefaultRandomizer()
-            {
-                random = new System.Random();
-            }
-
-            public int[] Integer(int min, int max, int count)
-            {
-                Task<int[]> results = IntegerAsync(min, max, count);
-                results.Wait();
-                return results.Result;
-            }
-
-            public async Task<int[]> IntegerAsync(int min, int max, int count)
-            {
-                int[] results = await Task.Run(() => new int[count]);
-                for(int i=0; i<count; i++)
-                {
-                    results[i] = random.Next(min, max);
-                }
-
-                return results;
-            }
-        }
-
         private class ParseResult
         {
             public int DieCount;
